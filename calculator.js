ee28efbcfb2fff2bbf631792bdedb9157842229d
amaypip481 <179745277+amaypip481@users.noreply.github.com>
2024-10-25 12:52:11 +0530

    fixes bug where typing numbers afte equal operator performed olf operation

--- calculator.js
+++ calculator.js
@@ -1,6 +1,8 @@
 let screenNumber = "";
 let savedAnswer = 0;
 let currentOperator = "";
+let isEqualGlobal = false;
+
 
 
 //Initializes calculator. can be later used for keeping a CE buttion
@@ -20,67 +22,85 @@ function initializeCalculator() {
 
 }
 
+function checkStringLength(numberString){
+    if (numberString.length >= 15) {
+        alert("This calculator only take 15 digits")
+        return true;
+    }
+
+    else return false;
+}
+
 //Numberbuttions eventlistener functions
 
 function eventListenerForNumberBtn(e) {
     e.srcElement.style.opacity = "0.7";
     setTimeout((e) => { numberButtonOpacityAnimation(e) }, 200);
-    if (screenNumber.length >= 15) {
-        alert("This calculator only take 15 digits")
-        return;
-    }
+    if(isEqualGlobal === true) currentOperator = "";
+
     switch (e.srcElement.id) {
         case "oneButton":
+            if(checkStringLength(screenNumber)) return;
             screenNumber += "1";
             document.querySelector(".displayDiv").firstElementChild.textContent = screenNumber;
             break;
 
         case "twoButton":
+            if(checkStringLength(screenNumber)) return;
             screenNumber += "2";
             document.querySelector(".displayDiv").firstElementChild.textContent = screenNumber;
             break;
 
         case "threeButton":
+            if(checkStringLength(screenNumber)) return;
             screenNumber += "3";
             document.querySelector(".displayDiv").firstElementChild.textContent = screenNumber;
             break;
 
         case "fourButton":
+            if(checkStringLength(screenNumber)) return;
             screenNumber += "4";
             document.querySelector(".displayDiv").firstElementChild.textContent = screenNumber;
             break;
 
         case "fiveButton":
+            if(checkStringLength(screenNumber)) return;
             screenNumber += "5";
             document.querySelector(".displayDiv").firstElementChild.textContent = screenNumber;
             break;
 
         case "sixButton":
+            if(checkStringLength(screenNumber)) return;
             screenNumber += "6";
             document.querySelector(".displayDiv").firstElementChild.textContent = screenNumber;
             break;
 
         case "sevenButton":
+            if(checkStringLength(screenNumber)) return;
             screenNumber += "7";
             document.querySelector(".displayDiv").firstElementChild.textContent = screenNumber;
             break;
 
         case "eightButton":
+            if(checkStringLength(screenNumber)) return;
             screenNumber += "8";
             document.querySelector(".displayDiv").firstElementChild.textContent = screenNumber;
             break;
 
         case "nineButton":
+            if(checkStringLength(screenNumber)) return;
             screenNumber += "9";
             document.querySelector(".displayDiv").firstElementChild.textContent = screenNumber;
             break;
 
         case "zeroButton":
+            if(checkStringLength(screenNumber)) return;
             screenNumber += "0";
             document.querySelector(".displayDiv").firstElementChild.textContent = screenNumber;
             break;
         case "decimalButton":
             if (screenNumber.includes(".")) break;
+            if(checkStringLength(screenNumber)) return;
             screenNumber += ".";
             document.querySelector(".displayDiv").firstElementChild.textContent = screenNumber;
             break;
@@ -113,11 +133,19 @@ function eventListenerForNumberBtn(e) {
             break;
 
         case "equalButton":
+            if (currentOperator === "") {
+                if (screenNumber === "") break;
+                savedAnswer = parseFloat(screenNumber);
+                screenNumber = "";
+                document.querySelector(".displayDiv").firstElementChild.textContent = screenNumber;
+        
+            }
             if (screenNumber === ".") {
                 screenNumber = "";
                 document.querySelector(".displayDiv").firstElementChild.textContent = screenNumber;
                 break;
             }
+            isEqualGlobal = true;
             operations(currentOperator, true);
             break;
 
@@ -143,6 +171,7 @@ function eventListenerForOperatorBtn(e) {
         document.querySelector(".displayDiv").firstElementChild.textContent = screenNumber;
 
     }
+    isEqualGlobal = false;
     operations(currentOperator, false);
     currentOperator = e.srcElement.id;
 }
@@ -157,18 +186,21 @@ function operations(previousOperator, isEqual) {
             savedAnswer = Math.round((savedAnswer + parseFloat(screenNumber)) * (Math.pow(10,10))) /( Math.pow(10,10));
             screenNumber = "";
             if (!isEqual) currentOperator = "";
+
             break;
         case "subtractButton":
             if (screenNumber === "") break;
             savedAnswer = Math.round((savedAnswer - parseFloat(screenNumber)) * Math.pow(10,10)) / Math.pow(10,10);
             screenNumber = "";
             if (!isEqual) currentOperator = "";
+   
             break;
         case "multiplyButton":
             if (screenNumber === "") break;
             savedAnswer = Math.round((savedAnswer * parseFloat(screenNumber)) * Math.pow(10,10)) / Math.pow(10,10);
             screenNumber = "";
             if (!isEqual) currentOperator = "";
+
             break;
         case "divideButton":
             if (screenNumber === "") break;
@@ -182,7 +214,7 @@ function operations(previousOperator, isEqual) {
             break;
 
     }
-    document.querySelector(".displayDiv").firstElementChild.textContent = savedAnswer;
+ document.querySelector(".displayDiv").firstElementChild.textContent = savedAnswer;
 }
 
 //Specific function for Percentage
@@ -203,62 +235,74 @@ function convertToPerceantge() {
 document.addEventListener("keydown", keyPressed);
 
 function keyPressed(e) {
-    if (screenNumber.length >= 15) {
-        alert("This calculator only take 15 digits")
-        return;
-    }
+  
     switch (e.key) {
         case "1":
+            if(checkStringLength(screenNumber)) return;
             screenNumber += "1";
             document.querySelector(".displayDiv").firstElementChild.textContent = screenNumber;
             break;
 
         case "2":
+            if(checkStringLength(screenNumber)) return;
             screenNumber += "2";
             document.querySelector(".displayDiv").firstElementChild.textContent = screenNumber;
             break;
 
         case "3":
+            if(checkStringLength(screenNumber)) return;
             screenNumber += "3";
             document.querySelector(".displayDiv").firstElementChild.textContent = screenNumber;
             break;
 
         case "4":
+            if(checkStringLength(screenNumber)) return;
             screenNumber += "4";
             document.querySelector(".displayDiv").firstElementChild.textContent = screenNumber;
             break;
 
         case "5":
+            if(checkStringLength(screenNumber)) return;
             screenNumber += "5";
             document.querySelector(".displayDiv").firstElementChild.textContent = screenNumber;
             break;
 
         case "6":
+            if(checkStringLength(screenNumber)) return;
             screenNumber += "6";
             document.querySelector(".displayDiv").firstElementChild.textContent = screenNumber;
             break;
 
         case "7":
+            if(checkStringLength(screenNumber)) return;
             screenNumber += "7";
             document.querySelector(".displayDiv").firstElementChild.textContent = screenNumber;
             break;
 
         case "8":
+            if(checkStringLength(screenNumber)) return;
             screenNumber += "8";
             document.querySelector(".displayDiv").firstElementChild.textContent = screenNumber;
             break;
 
         case "9":
+            if(checkStringLength(screenNumber)) return;
             screenNumber += "9";
             document.querySelector(".displayDiv").firstElementChild.textContent = screenNumber;
             break;
 
         case "0":
+            if(checkStringLength(screenNumber)) return;
             screenNumber += "0";
             document.querySelector(".displayDiv").firstElementChild.textContent = screenNumber;
             break;
         case ".":
             if (screenNumber.includes(".")) break;
+            if(checkStringLength(screenNumber)) return;
+            if (screenNumber.length >= 15) {
+                alert("This calculator only take 15 digits")
+                return;
+            }
             screenNumber += ".";
             document.querySelector(".displayDiv").firstElementChild.textContent = screenNumber;
 
@@ -269,6 +313,13 @@ function keyPressed(e) {
 
         case "=":
         case "Enter":
+            if (currentOperator === "") {
+                if (screenNumber === "") break;
+                savedAnswer = parseFloat(screenNumber);
+                screenNumber = "";
+                document.querySelector(".displayDiv").firstElementChild.textContent = screenNumber;
+        
+            }
             if (screenNumber === ".") {
                 screenNumber = "";
                 document.querySelector(".displayDiv").firstElementChild.textContent = screenNumber;
@@ -294,6 +345,7 @@ function keyPressed(e) {
                 document.querySelector(".displayDiv").firstElementChild.textContent = screenNumber;
 
             }
+        
             operations(currentOperator, false);
             if (e.key === "+") currentOperator = "addButton";
             else if (e.key === "-") currentOperator = "subtractButton";
